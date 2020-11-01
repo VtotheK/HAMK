fd91e8a17709311611fa8e08d610427569061e3f
vtothek <keitsi90@hotmail.com>
2020-11-01 04:11:39 -0800

    working on assignment

--- webkehitys/tehtava5/index.js
+++ webkehitys/tehtava5/index.js
@@ -1,6 +1,11 @@
 window.onload = function(){
-    var y = (screen.height/100) * 70
-    var x = (screen.width/100) * 80
-    document.getElementById("innercontainer").style.height= y + "px"
-    document.getElementById("innercontainer").style.width= x + "px"
+    var cont = document.getElementById("innercontainer");
+    var logo = document.getElementById("logo");
+    var y = (screen.height/100) * 70;
+    var x = (screen.width/100) * 80;
+
+    cont.style.height= y + "px"
+    cont.style.width= x + "px"
+    logo.style.left = (screen.width / 2) - 150 + "px"
+    logo.style.top =  10 + "px"
 }
