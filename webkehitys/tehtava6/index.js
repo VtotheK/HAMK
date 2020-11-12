f4fb612a3759fc9a9ce77f2aa7e7331250d3d39c
vtothek <keitsi90@hotmail.com>
2020-11-12 14:02:44 -0800

    all works fine

--- webkehitys/tehtava6/index.js
+++ webkehitys/tehtava6/index.js
@@ -1,10 +1,18 @@
-
-
+var currentimage = 0;
+var imagepaths = ["img/test.jpg","img/test2.jpg"];
 
 function nextimg(){
-
-
+    var imgs = document.getElementById("slideshow");
+    if(++currentimage >= imagepaths.length){
+        currentimage = 0;
+    }
+    imgs.src = imagepaths[currentimage];
 }
-function previousimg(){
 
+function previousimg(){
+    var imgs = document.getElementById("slideshow");
+    if(--currentimage < 0){
+        currentimage = imagepaths.length - 1; 
+    }
+    imgs.src = imagepaths[currentimage];
 }
