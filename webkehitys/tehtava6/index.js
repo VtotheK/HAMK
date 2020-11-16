b4e26f54c4dc51389e9e71b63d207212b2d73780
vtothek <keitsi90@hotmail.com>
2020-11-16 14:44:53 -0800

    facttsss

--- webkehitys/tehtava6/index.js
+++ webkehitys/tehtava6/index.js
@@ -1,5 +1,15 @@
 var currentimage = 0;
 var imagepaths = ["img/test.jpg","img/test2.jpg"];
+var curfact = 0;
+function randomFact(){
+    var len = facts.length;
+    var rndfact = curfact;
+    while(rndfact == curfact){
+        rndfact = Math.floor(Math.random() * len);
+    }
+    document.getElementById("randomtext").innerHTML = facts[rndfact]
+    curfact = rndfact;
+}
 
 function nextimg(){
     var imgs = document.getElementById("slideshow");
