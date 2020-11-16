799c8051412b7343f6e4aeced5928d01baf213a1
vtothek <keitsi90@hotmail.com>
2020-11-16 14:35:04 -0800

    fact

---
+++ webkehitys/tehtava6/facts.js
@@ -0,0 +1,6 @@
+var facts = ["If you read Gaunter O'Dimm's names first letters, it spells GOD, which he is.",
+            "In Witcher 3, there are 36 different endings.",
+            "Geralt meets Gaunter O'Dimm first time in White Orchard, at the beginning of the game.",
+            "If you play at higher difficulty, Geralt can't recharge health by meditating.",
+            "You can get one of the best weapons in the game by saving Olgierd von Everec's life.",
+            "The card game Gwent became so popular, that CD Projekt Red made it a standalone game."]
