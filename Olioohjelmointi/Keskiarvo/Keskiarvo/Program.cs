6a36b65aad93ba08183f8de8e1506f80b07307eb
VtotheK <ville.keituri1@gmail.com>
2021-01-17 00:26:30 +0200

    hardest stuff

---
+++ Olioohjelmointi/Keskiarvo/Keskiarvo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keskiarvo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count,sum = 0;
+            float avg;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Montako lukua");
+            }
+            while (!Int32.TryParse(Console.ReadLine(), out count));
+
+            for (int i = 0; i < count; i++)
+            {
+                int val;
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Anna luku {i+1}");
+                }
+                while (!Int32.TryParse(Console.ReadLine(), out val));
+                sum += val;
+            }
+
+            avg = sum / (float)count;
+            Console.WriteLine($"Average of your numbers are {avg}");
+            Console.ReadLine();
+        }
+    }
+}
