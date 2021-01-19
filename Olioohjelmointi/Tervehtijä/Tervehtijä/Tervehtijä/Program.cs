dfe38bc82dabce90d90f0e5a6fc28b3bed0927a5
VtotheK <ville.keituri1@gmail.com>
2021-01-20 00:22:58 +0200

    hardest stuff

---
+++ Olioohjelmointi/Tervehtijä/Tervehtijä/Tervehtijä/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tervehtijä
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tervehtijä t = new Tervehtijä();
+            t.AsetaTervehdys("Terve terve");
+            t.Tervehdi();
+            t.Tervehdi("Tässä oma tervehdys");
+            Console.WriteLine("Haettu tervehdys: {0}",t.HaeTervehdys());
+            Console.ReadLine();
+        }
+    }
+    
+    class Tervehtijä
+    {
+        string _tervehdys;
+
+        public void AsetaTervehdys(string uusiTervehdys)
+        {
+            _tervehdys = uusiTervehdys;
+        }
+
+        
+        public string HaeTervehdys()
+        {
+            return _tervehdys;
+        }
+
+        public void Tervehdi()
+        {
+            Console.WriteLine(_tervehdys);
+        }
+        public void Tervehdi(string omaTervehdys)
+        {
+            Console.WriteLine("Oma tervehdyksesi: {0}", omaTervehdys);
+        }
+
+    }
+}
